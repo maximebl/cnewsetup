809b887a47c587c6f1cc5954f9eeefe1312c6554
Maxime Bruneau-Lavoie <maximebl@videotron.ca>
2020-03-17 23:04:33 -0400

    cleaning up unused files

--- source/WinPixEventRuntime/cwinpix.h
+++
@@ -1,6 +0,0 @@
-__declspec(dllexport) void cPIXBeginEvent(UINT64 color, const char* label);
-__declspec(dllexport) void cPIXEndEvent();
-__declspec(dllexport) UINT cPIX_COLOR(BYTE r, BYTE g, BYTE b); 
-__declspec(dllexport) void cPIXNotifyWakeFromFenceSignal(HANDLE event);
-__declspec(dllexport) void cPIXBeginEvent_gpu(ID3D12CommandQueue* commandList, UINT64 color, const char* label);
-__declspec(dllexport) void cPIXEndEvent_gpu(ID3D12CommandQueue* commandList);
